a89965a23b6e451c0b3121fc278014d0891406ad
Michelle Hughes <michelle@iheartmichelle.com>
2024-11-18 09:44:45 -0500

    The whole bell aparatus is its own component.

---
+++ src/NotificationBell.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function NotificationBell() {
+  const [notificationsVisible, setNotificationsVisible] = useState(false);
+  const notificationsBoxRef = useRef(null);
+  const notificationsBoxCheckboxRef = useRef(null);
+  const notificationsBoxBellRef = useRef(null);
+
+  useEffect(() => {
+    function escKeyHandler(e) {
+      if (e.key == 'Escape') {
+        setNotificationsVisible(false);
+      }
+    }
+
+    if (notificationsVisible) {
+      window.addEventListener('keyup', escKeyHandler);
+    }
+
+    return () => {
+      window.removeEventListener('keyup', escKeyHandler); 
+    };
+    
+  }, [notificationsVisible, setNotificationsVisible]);
+
+/*
+  useEffect(() => {
+    const closeNotificationsBox = (e) => {
+      if (
+          !notificationsBoxCheckboxRef.current.contains(e.target)
+          && !notificationsBoxBellRef.current.contains(e.target)
+
+&& !notificationsBoxRef.current.contains(e.target)
+
+)
+      {
+        setNotificationsVisible(false);
+      }
+    };
+
+    window.addEventListener('click', closeNotificationsBox);
+
+    return () => { 
+      window.removeEventListener('click', closeNotificationsBox); 
+    };
+  }, [setNotificationsVisible]);
+*/
+
+
+  return (
+    <>
+      <input id="notifications-checkbox" type="checkbox"
+        ref={notificationsBoxCheckboxRef}
+        checked={notificationsVisible}
+        aria-label="No new notifications"
+        onChange={(e) => {
+          setNotificationsVisible(e.target.checked);
+         }}
+      />
+      <label htmlFor="notifications-checkbox" id="notifications-bell" ref={notificationsBoxBellRef}>🔔</label>
+
+      {notificationsVisible ? 
+        <section id="notifications" ref={notificationsBoxRef}>
+        <h2 id="notifications-header">Notifications</h2>
+        <ul aria-labelledby="notifications-header">
+          <li>No new notifications</li>
+        </ul>
+        </section>
+        :
+        ''
+      }
+    </>
+  );
+}
--- src/UserSection.jsx
+++ src/UserSection.jsx
@@ -1,35 +1,20 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from './UserContext.jsx';
 import { Link } from 'react-router-dom';
-import Notifications from './Notifications.jsx';
+import NotificationBell from './NotificationBell.jsx';
 
 import { User } from './logic/user.js';
 
 export default function UserSection() {
   const {user, setUser} = useContext(UserContext);
-  const [notificationsVisible, setNotificationsVisible] = useState(false);
-  const notificationsBoxRef = useRef(null);
 
   return (
     <section id="user-section">
       <ul>
-      {user ? 
+      {user ?
         <>
-        <li id="notifications" aria-label="No new notifications">
-          <input id="notifications-checkbox" type="checkbox" checked={notificationsVisible}
-            onChange={(e) => {
-              setNotificationsVisible(e.target.checked);
-             }}
-          />
-          <label htmlFor="notifications-checkbox" id="notifications-bell">🔔</label>
-          {notificationsVisible ? 
-            <Notifications
-                ref={notificationsBoxRef}
-                setNotificationsVisible={setNotificationsVisible}
-            />
-            :
-            ''
-          }
+        <li id="notification-bell">
+          <NotificationBell />
         </li>
         <li id="user-name">{user.name}</li>
         </>
