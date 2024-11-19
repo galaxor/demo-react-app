e8e938c2c7d53b741dd36d2775b1e73499e0cde6
Michelle Hughes <mhughes@19thnews.org>
2024-11-19 15:49:19 -0500

    System Notifications on more pages.

--- src/PopularFeed.jsx
+++ src/PopularFeed.jsx
@@ -1,7 +1,10 @@
+import SystemNotificationArea from './SystemNotificationArea.jsx';
+
 export default function PopularFeed() {
   return (
     <main>
       <h1 id="featured-feed">Popular Posts</h1>
+      <SystemNotificationArea />
       <ul aria-describedby="featured-feed">
         <li><article>Test post 1 by person</article></li>
         <li><article>Test post 2 by other person</article></li>
--- src/YourFeed.jsx
+++ src/YourFeed.jsx
@@ -1,7 +1,10 @@
+import SystemNotificationArea from './SystemNotificationArea.jsx';
+
 export default function YourFeed() {
   return (
     <main>
       <h1 id="your-feed">Your Feed</h1>
+      <SystemNotificationArea />
       <ul aria-describedby="your-feed">
         <li><article>Test post 1 by friend</article></li>
         <li><article>Test post 2 by other friend</article></li>
