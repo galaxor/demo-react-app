a2220d044d868e5e38170f100d4cefa57c26ee5f
Michelle Hughes <mhughes@19thnews.org>
2024-11-26 18:58:51 -0500

    The number of replies is correct.

--- src/NumReplies.jsx
+++ src/NumReplies.jsx
@@ -8,9 +8,12 @@ export default function NumReplies({post, knownReplies}) {
   const postsDB = new PostsDB(db);
   const replies = knownReplies ?? postsDB.getRepliesTo(post.uri);
 
-  const [numReplies, setNumReplies] = useState();
+  const [numReplies, setNumReplies] = useState(null);
 
   return (
-    <span className="num-replies">Replies: 0</span>
+    <span className="num-replies" aria-label="Replies">
+      <span className="num-replies-icon">💬</span>
+      <span className="num-replies-num">{numReplies ?? replies.length}</span>
+    </span>
   );
 }
