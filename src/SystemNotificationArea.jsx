a235c154d0110ac86c5a0c27144af80260678e88
Michelle Hughes <mhughes@19thnews.org>
2024-12-03 00:20:29 -0500

    More comfortable colors on alt text.

--- src/SystemNotificationArea.jsx
+++ src/SystemNotificationArea.jsx
@@ -8,14 +8,16 @@ export default function SystemNotificationArea() {
 
   const notifications = systemNotifications.map(item => {
     return (
-      <li key={item.uuid} className="item.level" role={item.level == 'error'? "alert" : "status"}>
-        <div className="message">{item.message}</div>
-        <a aria-label="Close this notification"
-          href=""
-          onClick={(e) => {
-            e.preventDefault();
-            setSystemNotifications(systemNotifications.filter(notification => notification.uuid != item.uuid));
-        }}>×</a>
+      <li key={item.uuid} className="item.level">
+        <div role={item.level == 'error'? "alert" : "status"}>
+          <div className="message">{item.message}</div>
+          <a aria-label="Close this notification"
+            href=""
+            onClick={(e) => {
+              e.preventDefault();
+              setSystemNotifications(systemNotifications.filter(notification => notification.uuid != item.uuid));
+          }}>×</a>
+        </div>
       </li> 
     );
   });
