fce153aeeac31903d0533d199ded5bff5eea0d4a
Michelle Hughes <mhughes@19thnews.org>
2024-12-02 23:45:50 -0500

    I guess an ok side-by-side layout for images.

--- src/ImageList.jsx
+++ src/ImageList.jsx
@@ -14,10 +14,12 @@ export default function ImageList({post}) {
   if (!imageBucket || Object.keys(imageBucket).length <= 0) { return ""; }
 
   return (
-    <ul className="post-images">
-      {Object.entries(imageBucket).map(([fileName, {data: imageData, altText}]) => {
-        return ( <li key={fileName}><img src={imageData} alt={altText} /> {altText && <details><summary>Alt Text</summary>{altText}</details>}</li> );
-      })}
-    </ul>
+    <div className="post-images">
+      <ul className="post-images">
+        {Object.entries(imageBucket).map(([fileName, {data: imageData, altText}]) => {
+          return ( <li key={fileName}><img src={imageData} alt={altText} /> {altText && <details><summary>Alt Text</summary>{altText}</details>}</li> );
+        })}
+      </ul>
+    </div>
   );
 }
