ed29778121c370c2d6332d12b1af84b2ae322638
Michelle Hughes <michelle@iheartmichelle.com>
2024-12-05 02:05:26 -0500

    We're using NextUI and Tailwind now.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
 import './index.css'
 
 import App from './App.jsx'
@@ -138,6 +139,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </StrictMode>,
 )
