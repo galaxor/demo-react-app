05e6f43e5086abddd7bc22f2ca7c73a6346f9c6e
Michelle Hughes <mhughes@19thnews.org>
2024-12-06 18:41:31 -0500

    Clicking the menu doesn't take you to the postSingle page.

--- src/clickPost.js
+++ src/clickPost.js
@@ -8,12 +8,15 @@ export function clickPost(e) {
 
   // Find all links.
   const links = node.querySelectorAll('a');
-  
-  // Is the target a child of a link, or a link itself?
-  var node = e.target;
-  const linksThatContainTarget = Array.from(links.values().filter(link => link.contains(node)));
 
-  if (linksThatContainTarget.length > 0) {
+  // Is the target a child of a link, or a link itself?
+  const linksThatContainTarget = Array.from(links.values().filter(link => link.contains(e.target)));
+
+  // Do the same thing for the "more options" menu.
+  // Also find the "more options" menu.
+  const moreOptionsMenu = node.querySelector('li.more-options-menu');
+
+  if (linksThatContainTarget.length > 0 || moreOptionsMenu.contains(e.target)) {
     return;
   } else {
     var node = e.target;
