b69ae3a293a0d177875090857fde006149becb7a
Michelle Hughes <mhughes@19thnews.org>
2024-12-14 14:00:57 -0500

    We're using the corner graphic in the orthodox way.
    Now I want to try to collapse long reply chains so that they don't indent at each new reply.

--- src/corner-svg.jsx
+++ src/corner-svg.jsx
@@ -1,10 +1,9 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!-- Created with Inkscape (http://www.inkscape.org/) -->
-
+export default function Corner() {
+  return (
 <svg
-   width="14.093959mm"
-   height="11.979865mm"
-   viewBox="0 0 14.093959 11.979865"
+   width="14.09396mm"
+   height="11.979866mm"
+   viewBox="0 0 14.09396 11.979866"
    version="1.1"
    id="svg1"
    inkscape:version="1.3.2 (091e20e, 2023-11-25)"
@@ -39,11 +38,16 @@
      inkscape:label="Layer 1"
      inkscape:groupmode="layer"
      id="layer1"
-     transform="translate(-59.899328,-95.838926)">
+     transform="translate(-59.899327,-95.838924)">
     <path
-       style="vector-effect:non-scaling-stroke;fill:none;stroke:#000000;stroke-width:0.292078;stroke-dasharray:none;-inkscape-stroke:hairline"
+       style={{'vector-effect':'non-scaling-stroke', fill:'none',stroke:'#000000','stroke-width':0.292078,'stroke-dasharray':'none','-inkscape-stroke':'hairline'}}
        d="m 59.899327,95.838924 c 1.752449,6.808396 5.423539,10.320396 14.09396,11.979866"
        id="path1"
-       sodipodi:nodetypes="cc" />
+       sodipodi:nodetypes="cc"
+       inkscape:export-filename="path1.svg"
+       inkscape:export-xdpi="96"
+       inkscape:export-ydpi="96" />
   </g>
 </svg>
+  );
+}
