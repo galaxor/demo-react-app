37fd291ce3d933a9fa863aca2442b8ea9adbb4e2
Michelle Hughes <mhughes@19thnews.org>
2025-01-08 23:19:06 -0500

    Spoiler text can be imported into the text editor.

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,18 +1,51 @@
-import { useMemo, useState } from 'react'
+import {
+  MDXEditor,
+} from '@mdxeditor/editor'
 
-function expensive() {
-  console.log("Expensive operation");
-  return "Expensive operation";
+import {micromark} from 'micromark'
+import { fromMarkdown } from 'mdast-util-from-markdown'
+import {toMarkdown} from 'mdast-util-to-markdown'
+import { gfmStrikethrough, gfmStrikethroughHtml } from 'micromark-extension-gfm-strikethrough'
+import {gfmStrikethroughFromMarkdown, gfmStrikethroughToMarkdown} from 'mdast-util-gfm-strikethrough'
+
+import { spoilerSyntax, spoilerHtml } from "micromark-extension-inline-spoiler";
+
+import { spoilerFromMarkdown, spoilerToMarkdown } from 'mdast-util-inline-spoiler'
+
+import { spoilerPlugin } from './editor/plugins/spoiler/index.ts'
+
+const testText = "`hello` ||hello|| ~~hello~~";
+
+function Test2() {
+  return (
+    <MDXEditor markdown={testText} />
+  );
 }
 
 export default function Test() {
-  const nothingImportantInitial = useMemo(expensive, []);
+  const output = micromark(testText, {
+    extensions: [gfmStrikethrough(), spoilerSyntax()],
+    htmlExtensions: [gfmStrikethroughHtml(), spoilerHtml()]
+  })
 
-  const [nothingImportant, setNothingImportant] = useState(nothingImportantInitial);
+  const asHtml = <div id="direct-to-html"><pre>{output}</pre></div>;
 
-  return (<>
-    <p>{nothingImportant? nothingImportant.toString() : ""}</p>
+  const ast = fromMarkdown(testText, {
+    extensions: [gfmStrikethrough(), spoilerSyntax()],
+    mdastExtensions: [gfmStrikethroughFromMarkdown(), spoilerFromMarkdown]
+  })
 
-    <button onClick={e => setNothingImportant("cheap operation")}>Press</button>
-  </>);
+  console.log(ast);
+
+  const roundTrip = toMarkdown(ast, {
+    extensions: [gfmStrikethroughToMarkdown({}), spoilerToMarkdown({})],
+  })
+
+  return <>
+    {asHtml} {"\n"} {roundTrip}
+    <MDXEditor 
+      markdown={testText}
+      plugins={[spoilerPlugin()]}
+    />
+  </>;
 }
---
+++ src/editor/plugins/spoiler/index.ts
@@ -0,0 +1,67 @@
+import { realmPlugin } from '@mdxeditor/editor'
+import {EditorConfig, TextNode} from 'lexical';
+import {SpoilerNode as MdastSpoilerNode, spoilerFromMarkdown, spoilerToMarkdown} from 'mdast-util-inline-spoiler'
+import { spoilerSyntax, spoilerHtml } from "micromark-extension-inline-spoiler";
+import { addActivePlugin$, addExportVisitor$, addImportVisitor$, addLexicalNode$, addMdastExtension$, addSyntaxExtension$, addToMarkdownExtension$ } from '@mdxeditor/editor'
+
+
+class SpoilerNode extends TextNode {
+  static getType(): string {
+    return 'spoiler';
+  }
+
+  static clone(node: SpoilerNode): SpoilerNode {
+    return new SpoilerNode(node.__text, node.__key);
+  }
+
+  createDOM(config: EditorConfig): HTMLElement {
+    const textNode = super.createDOM(config);
+    const span = document.createElement('span');
+    span.className = 'spoiler';
+    span.appendChild(textNode);
+    return span;
+  }
+}
+
+function $createSpoilerNode(text: string) {
+  return new SpoilerNode(text);
+}
+
+
+function $isSpoilerNode(node: LexicalNode | null | undefined): node is SpoilerNode {
+  return node instanceof SpoilerNode;
+}
+
+const MdastSpoilerVisitor: MdastImportVisitor<MdastSpoilerNode> = {
+  testNode: 'spoiler',
+  visitNode({ mdastNode, actions }) {
+    actions.addAndStepInto($createSpoilerNode(mdastNode.value))
+  }
+}
+
+const LexicalSpoilerVisitor: LexicalExportVisitor<SpoilerNode, MdastSpoilerNode> = { 
+  testLexicalNode: $isSpoilerNode,
+  visitLexicalNode: ({ lexicalNode, mdastParent, actions }) => {
+    const textContent = lexicalNode.getTextContent();
+    const spoiler: MdastSpoilerNode = { type: 'spoiler', value: textContent }
+    actions.appendToParent(mdastParent, spoiler)
+  }
+} 
+
+
+/**
+ * A plugin that allows you to use ||spoilers||.
+ */
+export const spoilerPlugin = realmPlugin({
+  init(realm) {
+    realm.pubIn({
+      [addActivePlugin$]: 'spoiler',
+      [addMdastExtension$]: spoilerFromMarkdown,
+      [addSyntaxExtension$]: spoilerSyntax(),
+      [addImportVisitor$]: [MdastSpoilerVisitor],
+      [addLexicalNode$]: SpoilerNode,
+      [addExportVisitor$]: LexicalSpoilerVisitor,
+      [addToMarkdownExtension$]: spoilerToMarkdown,
+    });
+  }
+});
