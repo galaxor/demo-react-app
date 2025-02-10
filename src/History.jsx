6b79883ba5b6e28017cd752a16ef1a4d7e0a5486
Michelle Hughes <michelle@iheartmichelle.com>
2025-02-09 23:42:49 -0500

    Showing diffs of the edit history.
    But we need to include the ability to show which images were added/removedk, and changes in their alt text, too.

--- src/History.jsx
+++ src/History.jsx
@@ -1,9 +1,11 @@
 import {Accordion, AccordionItem} from "@nextui-org/accordion";
 import { Button } from "@nextui-org/button"
+import { diffWordsWithSpace } from 'diff'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import TimeAgo from 'javascript-time-ago'
 import { useContext, useState } from 'react';
 import { useLoaderData } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid"
 
 import DatabaseContext from './DatabaseContext.jsx'
 import icons from './icons.js'
@@ -47,12 +49,45 @@ export default function History() {
 
       const humanReadableTime = <time dateTime={updatedAt}>{formattedTime}</time>;
 
-      return (
-      <AccordionItem key={updatedAt} aria-label={formattedTime} title={humanReadableTime}>
-        <Post showStats={false} post={postVersion} />
-      </AccordionItem>
-      );
-    })}
+      return ([
+        updatedAt,
+        formattedTime,
+        humanReadableTime,
+        postVersion,
+        <AccordionItem key={updatedAt} aria-label={formattedTime} title={humanReadableTime}>
+          <Post showStats={false} post={postVersion} />
+        </AccordionItem>
+      ]);
+    }).reduce((state, [updatedAt, formattedTime, humanReadableTime, postVersion, accordionItem]) => {
+      if (state.length === 0) {
+        return [...state, accordionItem, postVersion];
+      } else {
+        
+        const prevPost = state[state.length-1];
+
+        const accordionItemDiff = 
+          <AccordionItem key={`changes-${updatedAt}`} aria-label={`Changes introduced at ${formattedTime}`} title={<>Changes introduced {humanReadableTime}</>}>
+            {diffWordsWithSpace(prevPost.text, postVersion.text).map(part => {
+              if (part.added) {
+                return <ins key={uuidv4()}>{part.value}</ins>;
+              } else if (part.removed) {
+                return <del key={uuidv4()}>{part.value}</del>;
+              } else {
+                return part.value;
+              }
+            })}
+          </AccordionItem>
+        ;
+
+        // We peel off the previous postVersion from the state and then stick
+        // our own postVersion so the next iteration can read it as the
+        // "previous postVersion", so it can diff them.
+        // Make sure to use slice(0,-1) to peel the final "post version" off
+        // when passing the state array off to React to render.  We only want
+        // AccordionItems in the state array that we give to React.
+        return [...state.slice(0, -1), accordionItemDiff, accordionItem, postVersion];
+      }
+    }, []).slice(0,-1)}
     </Accordion>
   </>;
 }
