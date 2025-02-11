029b3cac241fc5ac8dead6b85ebec2809cbe3e6a
Michelle Hughes <michelle@iheartmichelle.com>
2025-02-11 01:44:44 -0500

    Teased out the data on added/removed images.
    We are not displaying the actual images yet, but we're saying if they've been added or removed.
    
    We also aren't yet displaying the changes to the alt text.

--- src/History.jsx
+++ src/History.jsx
@@ -1,11 +1,12 @@
 import {Accordion, AccordionItem} from "@nextui-org/accordion";
 import { Button } from "@nextui-org/button"
-import { diffWordsWithSpace } from 'diff'
+import { diffArrays, diffWordsWithSpace } from 'diff'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import TimeAgo from 'javascript-time-ago'
 import { useContext, useState } from 'react';
 import { useLoaderData } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid"
+import {Fragment} from 'react'
 
 import DatabaseContext from './DatabaseContext.jsx'
 import icons from './icons.js'
@@ -49,33 +50,88 @@ export default function History() {
 
       const humanReadableTime = <time dateTime={updatedAt}>{formattedTime}</time>;
 
+      const imageBucket = postsDB.getImagesForPost(postVersion.uri, postVersion.updatedAt);
+
       return ([
         updatedAt,
         formattedTime,
         humanReadableTime,
-        postVersion,
-        <AccordionItem key={updatedAt} aria-label={formattedTime} title={humanReadableTime}>
+        {...postVersion, images: imageBucket},
+        <AccordionItem key={updatedAt} aria-label={formattedTime} title={humanReadableTime} className="bg-content1">
           <Post showStats={false} post={postVersion} />
         </AccordionItem>
       ]);
     }).reduce((state, [updatedAt, formattedTime, humanReadableTime, postVersion, accordionItem]) => {
+      // Right now, the images are indexed by filename.
+      // In order to do the diff and find out what images have been added or
+      // removed, I want a version of the images array that is indexed by image
+      // hash instead, since that's the more meaningful attribute.  If, in one
+      // version, they included an image, and in the next version, they
+      // unlinked that image but then re-uploaded the same image with a
+      // different filename, I don't want to be telling people that's a new
+      // image.
+      postVersion.imagesByHash = {};
+      for (const fileName in postVersion.images) {
+        const hash = postVersion.images[fileName].image;
+        postVersion.imagesByHash[hash] = {...postVersion.images[fileName], fileName};
+      }
+
       if (state.length === 0) {
         return [...state, accordionItem, postVersion];
       } else {
         
         const prevPost = state[state.length-1];
 
+        const prevImageHashes = Object.keys(prevPost.imagesByHash);
+        const imageHashes = Object.keys(postVersion.imagesByHash);
+        const imageDiff = diffArrays(prevImageHashes, imageHashes);
+
+        console.log(imageDiff);
+
         const accordionItemDiff = 
-          <AccordionItem key={`changes-${updatedAt}`} aria-label={`Changes introduced at ${formattedTime}`} title={<>Changes introduced {humanReadableTime}</>}>
-            {diffWordsWithSpace(prevPost.text, postVersion.text).map(part => {
-              if (part.added) {
-                return <ins key={uuidv4()}>{part.value}</ins>;
-              } else if (part.removed) {
-                return <del key={uuidv4()}>{part.value}</del>;
-              } else {
-                return part.value;
-              }
-            })}
+          <AccordionItem key={`changes-${updatedAt}`} aria-label={`Changes introduced at ${formattedTime}`} title={<>Changes introduced {humanReadableTime}</>} className="bg-content2">
+            <dl>
+              <dt>Text</dt>
+              <dd className="whitespace-pre-line">
+                {diffWordsWithSpace(prevPost.text, postVersion.text).map(part => {
+                  if (part.added) {
+                    return <ins key={uuidv4()}>{part.value}</ins>;
+                  } else if (part.removed) {
+                    return <del key={uuidv4()}>{part.value}</del>;
+                  } else {
+                    return part.value;
+                  }
+                })}
+              </dd>
+              {imageDiff.map(imageChange => {
+                if (imageChange.removed) {
+                  return imageChangeTable({
+                    key: `removed-${imageChange.value[0]}`,
+                    caption: imageChange.length > 1? <>Images removed</> : <>Image removed</>,
+                    imageHashes: imageChange.value,
+
+                    // we need to pass prevPost so that it looks there for the
+                    // images. The images have been removed, so they won't be
+                    // in postVersion.
+                    postVersion: prevPost,
+                  });
+                } else if (imageChange.added) {
+                  return imageChangeTable({
+                    key: `added-${imageChange.value[0]}`,
+                    caption: imageChange.length > 1? <>Images added</> : <>Image added</>,
+                    imageHashes: imageChange.value,
+                    postVersion,
+                  });
+                } else {
+                  return imageChangeTable({
+                    key: `unchanged-${imageChange.value[0]}`,
+                    caption: imageChange.length > 1? <>Unchanged images</> : <>Unchanged image</>,
+                    imageHashes: imageChange.value,
+                    postVersion,
+                  });
+                }
+              })}
+            </dl>
           </AccordionItem>
         ;
 
@@ -90,4 +146,26 @@ export default function History() {
     }, []).slice(0,-1)}
     </Accordion>
   </>;
+}
+
+function imageChangeTable({key, caption, imageHashes, postVersion}) {
+  return (
+    <table className="image-change" key={key}>
+      <caption>{caption}</caption>
+      <thead>
+        <th>Image</th>
+        <th>Alt text</th>
+      </thead>
+      <tbody>
+      {imageHashes.map(imageHash => {
+        return (
+          <tr key={imageHash}>
+            <td className="image">{imageHash}</td>
+            <td className="alt-text">{postVersion.imagesByHash[imageHash].altText}</td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  );
 }
