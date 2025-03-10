7a68d169179117fa8f1147f96705dfc74727e95c
Michelle Hughes <michelle@iheartmichelle.com>
2025-03-09 21:21:38 -0400

    A little demo api for the worker.

--- src/Test.jsx
+++ src/Test.jsx
@@ -8,6 +8,31 @@ import UserDB from './logic/user.js'
 
 import workerUrl from './workers/worker.js?worker&url'
 
+class PostOffice {
+  constructor(worker) {
+    this.worker = worker;
+    this.messageId = 0;
+    this.correspondents = {};
+
+    this.worker.addEventListener('message', event => this.responder(event.data));
+  }
+
+  async send(message, callback) {
+    const returnAddress = this.messageId++;
+    this.correspondents[returnAddress] = callback;
+    this.worker.postMessage({returnAddress, message});
+  }
+
+  responder(responseEnvelope) {
+    const recipient = this.correspondents[responseEnvelope.returnAddress];
+    const response = responseEnvelope.response;
+    if (typeof recipient === 'function') {
+      recipient(response);
+    }
+    delete this.correspondents[responseEnvelope.returnAddress];
+  }
+}
+
 export default function Test({dbConnection}) {
   useEffect(() => {
     (async () => {
@@ -19,8 +44,8 @@ export default function Test({dbConnection}) {
       
       const worker = new Worker(workerUrl, {type: 'module'});
       console.log(worker);
-
-      worker.postMessage("Boop");
+      const postOffice = new PostOffice(worker);
+      postOffice.send({command: 'ding', message: 'ding dung'}, response => console.log("RSP", response));
     })();
   }, []);
 
--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,7 +1,46 @@
-// importScripts(new URL('/src/logic/test.js', import.meta.url));
-import Doot from '../logic/test.js'
+import Database from '../logic/database.js'
 
-onmessage = event => {
-  Doot(event.data);
-  console.log("Doot", event.data);
-};
+
+class MyWorker {
+  constructor() {
+    this.db = new Database();
+    onmessage = async event => this.dispatcher(event);
+  }
+
+  async dispatcher(event) {
+    const envelope = event.data;
+    const message = envelope.message;
+
+    var response;
+
+    switch (message.command) {
+    case 'init':
+      response = await this.init(message.serverUrl);
+      break;
+
+    case 'ding':
+      response = await this.ding(message.message);
+      break;
+    }
+
+    console.log("Doot", event.data);
+    this.respond(envelope, response);
+  }
+
+  async init(serverUrl) {
+    await this.db.open(serverUrl);
+    return "ready";
+  }
+
+  ding(message) {
+    console.log("Ding", message);
+    return {ding: message};
+  }
+
+  respond(envelope, response) {
+    postMessage({returnAddress: envelope.returnAddress, response});
+  }
+}
+
+// Actually set up the handler.
+const w = new MyWorker();
