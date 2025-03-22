0ccf58d8447ebf2dca324cd1bad7410a75698815
Michelle Hughes <michelle@iheartmichelle.com>
2025-03-22 04:21:53 -0400

    Don't let user content set css classes.

--- src/components/DangerousHTML.jsx
+++ src/components/DangerousHTML.jsx
@@ -1,5 +1,11 @@
 import DOMPurify from 'dompurify'
 
+const DOMPurifyConfig = {
+  FORBID_ATTR: [
+    'class',
+  ],
+};
+
 export default function DangerousHTML({children}) {
-  return <span className="user-content" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(children)}} />;
+  return <span className="user-content" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(children, DOMPurifyConfig)}} />;
 }
