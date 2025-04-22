db5d0673a755329ef5c5b3c390350e63c261ccb5
Michelle Hughes <michelle@iheartmichelle.com>
2025-04-22 08:46:53 -0400

    Refactor for clarity. Also, parallellism is not available.
    I changed the name of chunkSource to knownChunks for clarity.
    
    Also, I thought you could use a "skip" parameter to load the next 3 pages at a time, but it turns out there is no "skip" parameter in the mastodon api.  You have to get a page before you can know the address of the next page.

--- src/include/backfill.js
+++ src/include/backfill.js
@@ -1,8 +1,4 @@
-export function backfillIteration({chunkSource, mastodonApi, apiUrl, limit, callback}) {
-  const parallellism = 3;
-
-  const knownChunks = [...chunkSource];
-
+export function backfillIteration({knownChunks, mastodonApi, apiUrl, limit, callback}) {
   const promises = [];
 
   const deletedChunks = [];
@@ -20,26 +16,23 @@ export function backfillIteration({chunkSource, mastodonApi, apiUrl, limit, call
       maxId: minId,
     };
 
-    for (var i=0; i<parallellism; i++) {
-      params.skip = i * limit;
-      promises.push(
-        mastodonApi.apiGet(apiUrl, params, {parsePaginationLinkHeader: true}).then(async ({pagination, body}) => {
-          // Make sure the data is actually taken up before we update our
-          // records of what's known.
-          await callback(body);
+    promises.push(
+      mastodonApi.apiGet(apiUrl, params, {parsePaginationLinkHeader: true}).then(async ({pagination, body}) => {
+        // Make sure the data is actually taken up before we update our
+        // records of what's known.
+        await callback(body);
 
-          // Update our records of what's known.
-          return await findHomeForChunk(chunkSource, pagination, body);
-        })
-      );
-    }
+        // Update our records of what's known.
+        return await findHomeForChunk(knownChunks, pagination, body);
+      })
+    );
   }
 
   return promises;
 }
 
 
-export function findHomeForChunk(chunkSource, pagination, body) {
+export function findHomeForChunk(knownChunks, pagination, body) {
   const newChunk = {
     maxId: pagination.prev.sinceId,
     minId: pagination.next.maxId,
@@ -49,39 +42,38 @@ export function findHomeForChunk(chunkSource, pagination, body) {
   // We'll go through the known chunks -- but not the copy we're using
   // for this loop, we're going back to the source.  If we have chunks
   // that overlap, merge them.
-  // Note that `knownChunks` is a shallow copy of `chunkSource`.
-  // Mutating chunkSource won't mutate knownChunks. But mutating any of
-  // the chunks will mutate them in the copy.  That's okay.  If we end
-  // up finishing a processing a chunk before we're done dispatching,
-  // then we'll be happy we allowed the chunks to mutate, because that
-  // will cut down on duplicated fetches.
+  // Note that mutating any of the chunks will mutate them for all subsequent
+  // calls to findHomeForChunks.  That's okay.  If we end up finishing a
+  // processing a chunk before we're done dispatching, then we'll be happy we
+  // allowed the chunks to mutate, because that will cut down on duplicated
+  // fetches.
 
   // Traverse the chunks backward so that if we delete any chunks, it
   // doesn't mess with the indexes of subsequent chunks.
-  var chunkSourceIndex;
+  var knownChunksIndex;
   var placed = false;
-  for (chunkSourceIndex=chunkSource.length-1; chunkSourceIndex>=0; chunkSourceIndex--) {
+  for (knownChunksIndex=knownChunks.length-1; knownChunksIndex>=0; knownChunksIndex--) {
     var replaced = false;
-    if (chunkSource[chunkSourceIndex].maxId >= newChunk.minId 
-        && chunkSource[chunkSourceIndex].minId <= newChunk.maxId)
+    if (knownChunks[knownChunksIndex].maxId >= newChunk.minId 
+        && knownChunks[knownChunksIndex].minId <= newChunk.maxId)
     {
       // The chunk we're testing overlaps the new chunk.
-      newChunk.minId = Math.min(chunkSource[chunkSourceIndex].minId, newChunk.minId);
-      newChunk.maxId = Math.max(chunkSource[chunkSourceIndex].maxId, newChunk.maxId);
+      newChunk.minId = Math.min(knownChunks[knownChunksIndex].minId, newChunk.minId);
+      newChunk.maxId = Math.max(knownChunks[knownChunksIndex].maxId, newChunk.maxId);
       replaced = true;
     }
 
     // If we replaced, let's delete the old chunk and try the next old
     // chunk.
     if (replaced) {
-      chunkSource.splice(chunkSourceIndex, 1);
+      knownChunks.splice(knownChunksIndex, 1);
     } else {
       // If we didn't replace a chunk:
       // If the new chunk should be after this one, splice it into
-      // chunkSource after this chunk and stop this loop:  We've placed
+      // knownChunks after this chunk and stop this loop:  We've placed
       // the new chunk.
-      if (chunkSource[chunkSourceIndex].minId < newChunk.maxId) {
-        chunkSource.splice(chunkSourceIndex+1, 0, newChunk);
+      if (knownChunks[knownChunksIndex].minId < newChunk.maxId) {
+        knownChunks.splice(knownChunksIndex+1, 0, newChunk);
         placed = true;
         break;
       }
@@ -94,8 +86,8 @@ export function findHomeForChunk(chunkSource, pagination, body) {
   if (placed === false) {
     // We made it all the way through without finding a chunk that the new
     // chunk goes after.  That must mean the new chunk goes at the beginning.
-    chunkSource.unshift(newChunk);
+    knownChunks.unshift(newChunk);
   }
 
-  return chunkSource;
+  return knownChunks;
 }
--- src/include/backfill.test.js
+++ src/include/backfill.test.js
@@ -1,71 +1,71 @@
 import {findHomeForChunk} from './backfill.js'
 
 test('Adds a chunk to the end', () => {
-  const chunkSource = [{minId: 1, maxId: 3}];
+  const knownChunks = [{minId: 1, maxId: 3}];
   const pagination = {prev: {sinceId: 7}, next: {maxId: 6}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 3}, {minId: 6, maxId: 7}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 3}, {minId: 6, maxId: 7}]);
 });
 
 test('Adds a chunk to the beginning', () => {
-  const chunkSource = [{minId: 6, maxId: 7}];
+  const knownChunks = [{minId: 6, maxId: 7}];
   const pagination = {prev: {sinceId: 3}, next: {maxId: 1}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 3}, {minId: 6, maxId: 7}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 3}, {minId: 6, maxId: 7}]);
 });
 
 test('Adds a chunk in the middle', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
   const pagination = {prev: {sinceId: 5}, next: {maxId: 4}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 5}, {minId: 7, maxId: 8}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 5}, {minId: 7, maxId: 8}]);
 });
 
 test('Adds the only chunk', () => {
-  const chunkSource = [];
+  const knownChunks = [];
   const pagination = {prev: {sinceId: 5}, next: {maxId: 4}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 4, maxId: 5}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 4, maxId: 5}]);
 });
 
 test('Expands the last chunk to the left', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
   const pagination = {prev: {sinceId: 7}, next: {maxId: 4}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 8}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 8}]);
 });
 
 test('Expands the last chunk to the right', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
   const pagination = {prev: {sinceId: 9}, next: {maxId: 7}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 7, maxId: 9}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 7, maxId: 9}]);
 });
 
 test('Expands the last chunk in both directions', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 7, maxId: 8}];
   const pagination = {prev: {sinceId: 9}, next: {maxId: 6}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 6, maxId: 9}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 6, maxId: 9}]);
 });
 
 test('Expands the middle chunk in both directions', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
   const pagination = {prev: {sinceId: 7}, next: {maxId: 4}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 7}, {minId: 9, maxId: 10}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 4, maxId: 7}, {minId: 9, maxId: 10}]);
 });
 
 test('Merge last two chunks', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
   const pagination = {prev: {sinceId: 9}, next: {maxId: 6}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 5, maxId: 10}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 2}, {minId: 5, maxId: 10}]);
 });
 
 test('Merge first two chunks', () => {
-  const chunkSource = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
+  const knownChunks = [{minId: 1, maxId: 2}, {minId: 5, maxId: 6}, {minId: 9, maxId: 10}];
   const pagination = {prev: {sinceId: 5}, next: {maxId: 1}};
   const body = "nothing";
-  expect(findHomeForChunk(chunkSource, pagination, body)).toStrictEqual([{minId: 1, maxId: 6}, {minId: 9, maxId: 10}]);
+  expect(findHomeForChunk(knownChunks, pagination, body)).toStrictEqual([{minId: 1, maxId: 6}, {minId: 9, maxId: 10}]);
 });
